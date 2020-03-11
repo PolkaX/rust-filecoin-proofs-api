8095aaf408a65ac5fc64bca5ea716cf93b02c567
zhang-dong-dong <2209940496@qq.com>
2020-03-11 14:27:42 +0800

    Add seal-pre-commit

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,72 @@ pub use filecoin_proofs_v1::types::{
     UnpaddedBytesAmount,
 };
 pub use filecoin_proofs_v1::SnarkProof;
+pub use filecoin_proofs_v1;
+use std::path::Path;
+
+#[derive(Clone, Copy)]
+pub enum ResponseStatus {
+    NoErr,
+    UnClassified,
+}
+
+impl Default for ResponseStatus {
+    fn default() -> Self {
+        ResponseStatus::UnClassified
+    }
+}
+
+#[derive(Clone, Copy, Default)]
+pub struct SealPreCommitResponse {
+    status_code: ResponseStatus,
+    comm_d: Commitment,
+    comm_r: Commitment,
+}
+
+pub fn seal_pre_commit<R, S, T>(
+    registered_proof: RegisteredSealProof,
+    cache_path: R,
+    in_path: S,
+    out_path: T,
+    prover_id: ProverId,
+    sector_id: SectorId,
+    ticket: Ticket,
+    piece_infos: &[PieceInfo],
+) -> SealPreCommitResponse
+where
+    R: AsRef<Path> + Clone,
+    S: AsRef<Path>,
+    T: AsRef<Path> + Clone,
+{
+    let mut response = SealPreCommitResponse::default();
+    let seal_pre_commit_phase1_output = seal::seal_pre_commit_phase1(
+        registered_proof,
+        cache_path.clone(),
+        in_path,
+        out_path.clone(),
+        prover_id,
+        sector_id,
+        ticket,
+        piece_infos,
+    );
+    match seal_pre_commit_phase1_output {
+        Ok(output) => {
+            let seal_pre_commit_phase2_output =
+                seal::seal_pre_commit_phase2(output, cache_path, out_path);
+            match seal_pre_commit_phase2_output {
+                Ok(output) => {
+                    response.status_code = ResponseStatus::NoErr;
+                    response.comm_d = output.comm_d;
+                    response.comm_r = output.comm_r;
+                }
+                Err(_) => {
+                    response.status_code = ResponseStatus::UnClassified;
+                }
+            }
+        }
+        Err(_) => {
+            response.status_code = ResponseStatus::UnClassified;
+        }
+    }
+    response
+}
